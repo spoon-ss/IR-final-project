9154b9a14b66d74362d3befb0f3b68430b540d05
AirJunda <32043249+AirJunda@users.noreply.github.com>
2020-05-09 20:08:27 -0400

    add translation api

--- flaskapp/service/translate_service.py
+++ flaskapp/service/translate_service.py
@@ -1,9 +1,47 @@
+from googletrans import Translator
+import jieba
+
+
 class TranslateService:
     CHINESE_OPTION = "chinese"
     ENGLISH_OPTION = "english"
+    
+    def __init__(self, ser_url='translate.google.com'):
+        self.translator = Translator(service_urls=[ser_url])
+     
+           
+    def translate(self,txt):
+        "Translate raw txt to English"   
+        output = self.translator.translate(txt)
+        return output.text.lower()
+    
+    def tokenizer_chn(self,txt):
+        "Tokenize Chinese querty to Chinese query"
+        result = list(jieba.cut_for_search(query))
+        return result
+    
+    def ctoks2Eng(self, chntoks):
+        "covert chinese tokens to english token"
+        result = []
+        for wd in chntoks:
+            result.append(self.translator.translate(wd).text.lower())
+        return result
+    
+    def translateTokens(self,txt):
+        "translate chinese raw text into enlgish tokens for searching purpose"
+        chntoks = self.tokenizer_chn(txt)
+        chntoks = self.ctoks2Eng(chntoks)
+        translated = self.ctoks2Eng(chntoks)
+        return translated
+    
+  
+  
+    
+    
 
-    def __init__(self):
-        pass
+        
 
-    def translate(self, query_str: str, from_lang: str, to_lang: str) -> str:
-        return query_str + " translated"
+    
+    
+    
+    
